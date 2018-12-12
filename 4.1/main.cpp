39a274c93a3534688eaea8930c89994cfed7ca15
jonochar <jonochar1@gmail.com>
2018-12-11 18:49:57 -0600

    4.1 is taking a long time

--- 4.1/main.cpp
+++ 4.1/main.cpp
@@ -5,8 +5,13 @@
 #include <map>
 #include <sstream>
 #include <boost/algorithm/string.hpp>
+#include <iomanip>
+#include <ctime>
 
 using namespace std;
+
+const int WAKES = -1;
+const int SLEEPS = -2;
 
 void parseAndSortFile(string fname);
 
@@ -35,21 +40,20 @@ void parseAndSortFile(string fname)
                 for (auto it = v.begin()+2; it != v.end(); it++) {
                     if (i < 5) {
                         timestamp += *it;
+                        if (i<4) timestamp += ":";
                     } else {
                         if (*it == "falls") {
-                            event = -1;
+                            event = SLEEPS;
                         } else if (*it == "wakes") {
-                            event = -2;
+                            event = WAKES;
                         } else if (*it == "Guard") {
                             it += 2;    // there's a null because of the #
                             stringstream ss(*it);
                             ss >> event;
                         }
                     }
-                    cout << *it;
                     i++;
                 }
-                cout << "\n";
                 sorted_events[timestamp] = event;
             }
         } catch (int e) {
@@ -59,10 +63,41 @@ void parseAndSortFile(string fname)
         }
         f.close();
     }
+    string timefmt = "%m:%d:%H:%M";
+    struct tm last_event, cur_event;
+    double slept = 0;
+    int cur_guard, eval;
+    map<int, double> asleep_total;
+    map<int, vector<int > > hist;
 
     for (auto it = sorted_events.begin(); it != sorted_events.end(); it++) {
+        timestamp = it->first;
+        istringstream ss(timestamp);
+        ss >> get_time(&cur_event, timefmt.c_str());
+        eval = it->second;
+        if (eval > 0) {
+            cur_guard = eval;
+        } else if (eval == SLEEPS) {
+            last_event = cur_event;
+        } else if (eval == WAKES) {
+            time_t cur_time = mktime(&cur_event);
+            time_t last_time = mktime(&last_event);
+            asleep_total[cur_guard] += difftime(cur_time, last_time);
+        } else {
+            cout << "\nerror\n";
+        }
+        //for debug:
         cout << it->first << ": " << it->second << endl;
     }
-
     cout << "This many loops: " << count << endl;
+
+    double max = -1;
+    int guard = -1;
+    for (auto it = asleep_total.begin(); it != asleep_total.end(); it++) {
+        if (it->second > max) max = it->second;
+        guard = it->first;
+    }
+    max = max/60;
+    cout << "Guard: " << guard << " sleeps the most at: " << max << " minutes\n";
+    cout << "Guard * minutes = " << guard * (int)max << endl;
 }
