dca9a8fce52012f84e29e510f0626d4ccef9871b
jonochar <jonochar1@gmail.com>
2018-12-14 17:50:44 -0600

    Finally finished 4.1

--- 4.1/main.cpp
+++ 4.1/main.cpp
@@ -30,36 +30,30 @@ void parseAndSortFile(string fname)
     vector<string> v;
     ifstream f(fname);
     if (f.is_open()) {
-        try {
-            while (getline(f, line)) {
-                count++;
-                boost::split(v, line, boost::is_any_of("[- :]#"));
-                int i = 1;
-                timestamp = "";
-                event = 0;
-                for (auto it = v.begin()+2; it != v.end(); it++) {
-                    if (i < 5) {
-                        timestamp += *it;
-                        if (i<4) timestamp += ":";
-                    } else {
-                        if (*it == "falls") {
-                            event = SLEEPS;
-                        } else if (*it == "wakes") {
-                            event = WAKES;
-                        } else if (*it == "Guard") {
-                            it += 2;    // there's a null because of the #
-                            stringstream ss(*it);
-                            ss >> event;
-                        }
+        while (getline(f, line)) {
+            count++;
+            boost::split(v, line, boost::is_any_of("[- :]#"));
+            int i = 1;
+            timestamp = "";
+            event = 0;
+            for (auto it = v.begin()+2; it != v.end(); it++) {
+                if (i < 5) {
+                    timestamp += *it;
+                    if (i<4) timestamp += ":";
+                } else {
+                    if (*it == "falls") {
+                        event = SLEEPS;
+                    } else if (*it == "wakes") {
+                        event = WAKES;
+                    } else if (*it == "Guard") {
+                        it += 2;    // there's a null because of the #
+                        stringstream ss(*it);
+                        ss >> event;
                     }
-                    i++;
                 }
-                sorted_events[timestamp] = event;
+                i++;
             }
-        } catch (int e) {
-            f.close();
-            cout << "\n\nError Error Error: \n\n";
-            throw e;
+            sorted_events[timestamp] = event;
         }
         f.close();
     }
@@ -77,27 +71,59 @@ void parseAndSortFile(string fname)
         eval = it->second;
         if (eval > 0) {
             cur_guard = eval;
+            if (hist[cur_guard].size() < 60) {
+                vector<int> minutes(60, 0);
+                hist[cur_guard] = minutes;
+            }
         } else if (eval == SLEEPS) {
             last_event = cur_event;
         } else if (eval == WAKES) {
             time_t cur_time = mktime(&cur_event);
             time_t last_time = mktime(&last_event);
             asleep_total[cur_guard] += difftime(cur_time, last_time);
+            for (int i = last_event.tm_min; i < cur_event.tm_min; i++) {
+                hist[cur_guard][i]++;
+            }
         } else {
             cout << "\nerror\n";
         }
-        //for debug:
-        cout << it->first << ": " << it->second << endl;
     }
-    cout << "This many loops: " << count << endl;
 
     double max = -1;
     int guard = -1;
     for (auto it = asleep_total.begin(); it != asleep_total.end(); it++) {
-        if (it->second > max) max = it->second;
-        guard = it->first;
+        if (it->second > max) {
+            guard = it->first;
+            max = it->second;
+        }
     }
+
+
+    for (auto it = hist.begin(); it != hist.end(); it++) {
+        int key = it->first;
+        cout << key << " < ";
+        for (int i=0; i<60; i++) {
+            cout << hist[key][i] << " ";
+        }
+        cout << " > " << asleep_total[key] << endl;
+    }
+
+    int best_min = -1;
+    int minute_val = -1;
+    cout << "Best guard: ";
+    cout << guard << " < ";
+    for (int i=0; i<60; i++) {
+        cout << hist[guard][i] << " ";
+        if (hist[guard][i] > minute_val) {
+            minute_val = hist[guard][i];
+            best_min = i;
+        }
+    }
+    cout << " > " << asleep_total[guard] << endl;
+
     max = max/60;
     cout << "Guard: " << guard << " sleeps the most at: " << max << " minutes\n";
-    cout << "Guard * minutes = " << guard * (int)max << endl;
+    cout << "This many loops: " << count << endl;
+    cout << "Best minute = " << best_min << endl;
+    cout << "guard*min: " << best_min * guard << endl;
 }
