637ac708bdcaef534e1da05374ac630ad40438e2
jonochar <jonochar1@gmail.com>
2018-12-19 14:35:19 -0600

    6.2

--- 6/main.cpp
+++ 6/main.cpp
@@ -21,7 +21,7 @@ int main()
     ifstream inputFile("input.txt");
     map<int, map<int, int> > points;
     string line;
-    int x, y, m_x =-1, m_y = -1, l_x = 500, l_y = 500;  // I can see nothing in input.txt exceeds 500
+    int x, y, m_x =-1, m_y = -1, l_x = 500, l_y = 500, threshold = 10000;  // I can see nothing in input.txt exceeds 500
     char c;
 
     if (inputFile.is_open()) {
@@ -39,9 +39,12 @@ int main()
     }
     
     vector<vector<int> > space(m_x, vector<int> (m_y));
-    int d, ld, p, md, ma=0;
+    int d, ld, p, md, ma=0, ca=0, td;
     auto besty = (points.begin()->second).begin();
     auto bestx = points.begin();
+    auto centy = besty;
+    auto centx = bestx;
+
     md = calcManhattan(l_x, l_y, m_x, m_y);
     cout << "Square size: " << m_x-l_x << ":" << m_y-l_y << " Max distance: " << md << endl;
 
@@ -51,9 +54,11 @@ int main()
             auto cury = (points.begin()->second).begin();
             auto curx = points.begin();
             p = 0;
+            td = 0;
             for (auto it = points.begin(); it != points.end(); it++) {
                 for (auto jt = (it->second).begin(); jt != (it->second).end(); jt++) {
                     d = calcManhattan(i, j, it->first, jt->first);
+                    td += d;
                     if (d <= ld) {
                         p = d == ld ? 0 : 1;
                         ld = d;
@@ -62,7 +67,7 @@ int main()
                     }
                 }
             }
-            
+            if (td < threshold) ca++;
             if (i == l_x || i == m_x || j == l_y || j == m_y) {
                 cury->second = -1;
             } else if (cury->second > -1) {
@@ -84,4 +89,5 @@ int main()
 
     cout << "Dim: " << m_x << " x " << m_y << ": " << m_x * m_y << endl;
     cout << "Greatest area: " << bestx->first << "," << besty->first << " : " << besty->second << endl;
+    cout << "Central Area: " << ca << endl;
 }
