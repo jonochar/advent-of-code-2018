91d98f1e3f905e9627f722769c5ed6c5b9885d04
jleal <jcleal91@gmail.com>
2018-12-22 11:46:27 -0600

    hmmm 7

--- 7/main.cpp
+++ 7/main.cpp
@@ -6,17 +6,26 @@
 #include <vector>
 #include <queue>
 #include <stack>
+#include <set>
 
 using namespace std;
 
-void handleEvents(stack <char>* st, map<char, map<char> >* graph, vector<char>* events, char cur)
+void handleEvents(stack <char>* st, map<char, set<char> >* graph, map<char, int>* visited, char cur)
 {
     if ((*graph)[cur].empty()) {
-        st->push(cur);
         return;
     }
 
+    if ((*visited)[cur] > 0) {
+        return;
+    }
+
+    for (auto it = (*graph)[cur].end(); it != (*graph)[cur].begin(); it--) {
+        handleEvents(st, graph, visited, *it);
+    }
     
+    (*visited)[cur] = 1;
+    st->push(cur);
 }
 
 int main()
@@ -24,26 +33,43 @@ int main()
     ifstream f("input.txt");
     string line, word;
 
-    int actionIdx, prereqIdx, i;
-    char action = 7, prereq = 1, step; //this only works because of pre-knowledge of the file format
-    map<char, map<char> > prereqs, unlocks;
+    int actionIdx = 7, prereqIdx = 1, i;
+    char action, prereq, start; //this only works because of pre-knowledge of the file format
+    map<char, set<char> > graph, igraph;
+    map<char, int> visited;
     vector<char> events;
     queue <char> q;
 
     if (f.is_open()) {
-        i=0;
         while(getline(f, line)) {
+            if (line == "\n") continue;
             stringstream words(line);
+            i=0;
             while(getline(words, word, ' ')) {
-                if (i == actionIdx) action = word[0];
-                if (i == prereqIdx) prereq = word[0];
+                if (i == actionIdx) {
+                    action = word[0];
+                } else if (i == prereqIdx) {
+                    prereq = word[0];
+                } 
                 i++;
             }
-            unlocks[prereq].insert(action);
-            prereqs[action].insert(prereq);
-            if (prereqs[action].size() == 0) step = prereq;
+            graph[prereq].insert(action);
+            igraph[action].insert(prereq);
+            if (igraph[action].size() == 0) start = action;
         }
     }
     stack<char> st;
-    handleEvents(&st, &unlocks, &events);
+    handleEvents(&st, &graph, &visited, start);
+    
+    cout << "Graph Size: " << graph.size() << endl;
+    for (auto it = graph.begin(); it != graph.end(); it++) {
+        cout << "Node: " << it->first << " has " << it->second.size() << "elements\n";
+    }
+
+    cout << "Order:\n";
+    while (!st.empty()) {
+        cout << st.top();
+        st.pop();
+    }
+    cout << endl;
 }
